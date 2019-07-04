e5490d64fe6af69d62236a56db237b6c4ba76e90
mick-roper <mick.roper@dazn.com>
2019-07-04 12:40:39 +0100

    added a simple server implementation

--- server/go.mod
+++ server/go.mod
@@ -1,3 +1,5 @@
 module github.com/mick-roper/strudel/server
 
 go 1.12
+
+require github.com/gorilla/websocket v1.4.0 // indirect
--- server/main.go
+++ server/main.go
@@ -1,7 +1,41 @@
 package main
 
-import "log"
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{}
+
+func echo(w http.ResponseWriter, r *http.Request) {
+	c, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Print("upgrade:", err)
+		return
+	}
+	defer c.Close()
+
+	for {
+		mt, message, err := c.ReadMessage()
+		if err != nil {
+			log.Println("read:", err)
+			break
+		}
+
+		log.Printf("recv: %s", message)
+		err = c.WriteMessage(mt, message)
+		if err != nil {
+			log.Println("write:", err)
+			break
+		}
+	}
+}
 
 func main() {
-	log.Print("hello, world!")
+	log.Print("starting server...")
+
+	http.HandleFunc("/", echo)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
