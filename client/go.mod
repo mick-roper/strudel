94f9aea1dd291062a6c89938416175db5a95bd29
mick-roper <mick.roper@dazn.com>
2019-07-04 12:27:28 +0100

    added a simple client implementation

--- client/go.mod
+++ client/go.mod
@@ -1,3 +1,5 @@
 module github.com/mick-roper/strudel/client
 
 go 1.12
+
+require github.com/gorilla/websocket v1.4.0 // indirect
--- client/main.go
+++ client/main.go
@@ -1,7 +1,75 @@
 package main
 
-import "log"
+import (
+	"log"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/gorilla/websocket"
+)
 
 func main() {
-	log.Print("hello, world")
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt)
+
+	c, _, err := websocket.DefaultDialer.Dial("", nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer c.Close()
+
+	done := make(chan struct{})
+
+	go func() {
+		defer close(done)
+
+		for {
+			_, message, err := c.ReadMessage()
+
+			if err != nil {
+				log.Println("read:", err)
+				return
+			}
+
+			log.Printf("recv: %s", message)
+		}
+	}()
+
+	ticker := time.NewTicker(time.Second)
+	defer ticker.Stop()
+
+	for {
+		select {
+		// break out
+		case <-done:
+			return
+		// on tick
+		case t := <-ticker.C:
+			err := c.WriteMessage(websocket.TextMessage, []byte(t.String()))
+			if err != nil {
+				log.Println("write:", err)
+				return
+			}
+			// on interrupt
+		case <-interrupt:
+			log.Println("interrupt received")
+
+			err := c.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalMessage, ""))
+
+			if err != nil {
+				log.Println("write close:", err)
+				return
+			}
+
+			// wait for the socket to be closed
+			select {
+			case <-done:
+			case <-time.After(time.Second):
+			}
+			return
+		}
+	}
 }
