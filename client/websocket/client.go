8d60b10e9d0edc775a78386c993202e8048d804a
mick-roper <mick.roper@dazn.com>
2019-07-05 14:47:51 +0100

    added a simple client

---
+++ client/websocket/client.go
@@ -0,0 +1,32 @@
+package websocket
+
+import (
+	"errors"
+
+	"github.com/gorilla/websocket"
+)
+
+// Client used to manage websocket connections
+type Client struct {
+	conn websocket.Conn
+}
+
+// NewClient creates a new client
+func NewClient(url string) (*Client, error) {
+	if url == "" {
+		return nil, errors.New("url must be provided")
+	}
+
+	conn, _, err := websocket.DefaultDialler.Dial(url, nil)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &Client{conn: conn}, nil
+}
+
+// Send a message using the client
+func (c *Client) Send(payload string) error {
+	return errors.New("not implemented")
+}
