a39f4c65412ee7ca1aa5920666236fa9e3b07060
mick-roper <mick.roper@dazn.com>
2019-07-05 15:03:01 +0100

    added some more null guards

--- client/websocket/client.go
+++ client/websocket/client.go
@@ -2,6 +2,7 @@ package websocket
 
 import (
 	"errors"
+	"log"
 
 	"github.com/gorilla/websocket"
 )
@@ -12,9 +13,13 @@ type Client struct {
 }
 
 // NewClient creates a new client
-func NewClient(url string) (*Client, error) {
+func NewClient(url string, messageReceived func(string)) (*Client, error) {
 	if url == "" {
 		return nil, errors.New("url must be provided")
+	}
+
+	if messageReceived == nil {
+		return nil, errors.New("no messageReceived function")
 	}
 
 	conn, _, err := websocket.DefaultDialer.Dial(url, nil)
@@ -23,10 +28,29 @@ func NewClient(url string) (*Client, error) {
 		return nil, err
 	}
 
+	go func() {
+		for {
+			_, bytes, err := conn.ReadMessage()
+
+			if err != nil {
+				log.Print(err)
+			}
+
+			str := string(bytes)
+
+			messageReceived(str)
+		}
+	}()
+
 	return &Client{conn: conn}, nil
 }
 
 // Send a message using the client
 func (c *Client) Send(payload string) error {
 	return errors.New("not implemented")
+}
+
+// Close the client
+func (c *Client) Close() {
+	c.conn.Close()
 }
