0055d63e759d10a4a61dc8ecc806adb870b0a990
mick-roper <mick.roper@dazn.com>
2019-07-05 15:06:52 +0100

    added some flags

--- client/app/client.go
+++ client/app/client.go
@@ -1,17 +1,22 @@
 package main
 
 import (
+	"flag"
 	"log"
 
 	"github.com/mick-roper/strudel/client/websocket"
 )
+
+var server = flag.String("server", "", "the strudel server")
 
 func messageRecieved(message string) {
 	log.Println(message)
 }
 
 func main() {
-	client, err := websocket.NewClient("abc", messageRecieved)
+	flag.Parse()
+
+	client, err := websocket.NewClient(*server, messageRecieved)
 
 	if err != nil {
 		log.Print(err)
